2dded9c07903251f2c2b61fda773594fa693743b
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2024-08-13 21:03:59 +0530

    add data folder

---
+++ src/data/data.js
@@ -0,0 +1,137 @@
+// Skills 
+import { FaCss3Alt, FaGitAlt } from "react-icons/fa6";
+import { RiTailwindCssLine, RiJavascriptFill } from "react-icons/ri";
+import { IoLogoReact } from "react-icons/io5";
+import { FaNode, FaJava } from "react-icons/fa";
+import { SiExpress, SiMongodb, SiFirebase } from "react-icons/si";
+import { TiHtml5 } from "react-icons/ti";
+import { BiLogoTypescript } from "react-icons/bi";
+
+
+
+// Tools 
+import { FcLinux } from "react-icons/fc";
+import { TbBrandVscode } from "react-icons/tb";
+import { SiPostman } from "react-icons/si";
+import { RxVercelLogo } from "react-icons/rx";
+import { SiIntellijidea } from "react-icons/si";
+
+
+//Footer 
+
+import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa6";
+
+
+
+
+
+// Skills 
+export const skillInfo = [
+    {
+        skillName: 'Core Java',
+        icon: FaJava,
+    },
+    {
+        skillName: 'JavaScript',
+        icon: RiJavascriptFill,
+
+    },
+    {
+        skillName: 'TypeScript',
+        icon: BiLogoTypescript,
+
+    },
+    {
+        skillName: 'React Js',
+        icon: IoLogoReact,
+
+    },
+    {
+        skillName: 'Tailwind Css',
+        icon: RiTailwindCssLine,
+
+    },
+    {
+        skillName: 'MongoDB',
+        icon: SiMongodb,
+
+    },
+    {
+        skillName: 'Node Js',
+        icon: FaNode,
+
+    },
+    {
+        skillName: 'Express Js',
+        icon: SiExpress,
+
+    },
+    {
+        skillName: 'Git',
+        icon: FaGitAlt,
+
+    },
+    {
+        skillName: 'FireBase',
+        icon: SiFirebase,
+
+    },
+    {
+        skillName: 'CSS3',
+        icon: FaCss3Alt,
+
+    },
+    {
+        skillName: 'HTML5',
+        icon: TiHtml5,
+
+    },
+]
+
+// Tools 
+export const toolsIUse = [
+    {
+        skillName: 'Linux',
+        icon: FcLinux,
+    },
+    {
+        skillName: 'Vs Code',
+        icon: TbBrandVscode,
+    },
+    {
+        skillName: 'PostMan',
+        icon: SiPostman,
+    },
+    {
+        skillName: 'Vercel',
+        icon: RxVercelLogo,
+    },
+    {
+        skillName: 'Intellij Idea',
+        icon: SiIntellijidea,
+    },
+];
+
+// Footer links 
+
+export const footerLinks = [
+    {
+        link: 'https://github.com/Rakesh-99',
+        icon: FaGithub
+
+    },
+    {
+        link: 'https://www.linkedin.com/in/rakesh-kumar-parida-b142081a9/',
+        icon: FaLinkedin
+    },
+    {
+        link: 'https://www.instagram.com/_rakesh_ku_parida/',
+        icon: FaInstagram
+    },
+    {
+        link: 'https://x.com/kumar_pari23020',
+        icon: FaTwitter
+    },
+
+]
