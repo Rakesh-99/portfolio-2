494212fbbb0375b4e8953b467c7c2f14cd0103b5
Rakesh Kumar Parida <rakeshkumarparida424@gmail.com>
2024-08-13 21:03:03 +0530

    add an extra download button and toast notification

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ResumePDF from '../assets/resume/RakeshUpdatedResume.pdf';
 import ResumeIMG from '../assets/resume/RakeshUpdatedResume.png';
 import { useSelector } from 'react-redux';
+import toast from 'react-hot-toast';
 
 
 
@@ -11,13 +12,18 @@ const Resume = () => {
     const { theme } = useSelector((state) => state.themeToggle);
 
 
+    const notify = () => {
+        setTimeout(() => {
+            toast.success('Thanks for downloading my Resume')
+        }, 1000);
+    }
 
     return (
 
         <div className="min-h-screen w-[90%] lg:w-[50%] md:w-[50%] m-auto my-5">
 
             <div className="my-10 text-center">
-                <button className={`border duration-300 text-sm font-semibold  shadow-md hover:-translate-y-1 rounded-md py-2 px-2  ${theme === 'dark' ? 'border-yellow-500 hover:bg-yellow-500' : ' border-blue-500 hover:bg-indigo-700 hover:text-white'}`}>
+                <button onClick={notify} className={`border duration-300 text-sm font-semibold  shadow-md hover:-translate-y-1 rounded-md py-2 px-2  ${theme === 'dark' ? 'border-yellow-500 hover:bg-yellow-500' : ' border-blue-500 hover:bg-indigo-700 hover:text-white'}`}>
                     <a href={ResumePDF} download='RakeshResumePdf'>Download CV</a>
                 </button>
             </div>
@@ -25,6 +31,13 @@ const Resume = () => {
             <div className={`showResume border rounded-2xl px-5 py-5 ${theme === 'dark' ? 'border-gray-700 shadow-2xl shadow-yellow-800' : 'border-gray-200 shadow-xl shadow-blue-300 '} `}>
                 <img src={ResumeIMG} alt="Resume" className='rounded-2xl' />
             </div>
+
+            <div className="w-full text-center my-10">
+                <button onClick={notify} className={`border duration-300 text-sm font-semibold  shadow-md hover:-translate-y-1 rounded-md py-2 px-2  ${theme === 'dark' ? 'border-yellow-500 hover:bg-yellow-500' : ' border-blue-500 hover:bg-indigo-700 hover:text-white'}`}>
+                    <a href={ResumePDF} download='RakeshResumePdf'>Download CV</a>
+                </button>
+            </div>
+
         </div>
     )
 }
