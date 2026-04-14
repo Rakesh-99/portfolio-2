0517849e4baab3d9497b84251a7db2b7eb1a760b
Rakesh-99 <rakeshkumarparida424@gmail.com>
2026-04-14 22:21:34 +0530

    feat : add lenis  for smooth scroll , and add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -8,48 +8,47 @@ import ThemeProvider from './components/ThemeProvider';
 import { Toaster } from 'react-hot-toast';
 import ScrollToTop from './components/ScrollToTop';
 import ParticlesComponent from './components/Particles';
-import persistStore from 'redux-persist/es/persistStore';
+import { persistStore } from 'redux-persist';  // ✅ named import
 import Spinner from './components/spinner/Spinner';
-
+import { ReactLenis } from '@studio-freight/react-lenis';
+import { Analytics } from '@vercel/analytics/react'
 
 const Home = lazy(() => import('./pages/Home'));
 const Contact = lazy(() => import('./pages/Contact'));
 const About = lazy(() => import('./pages/About'));
 const Projects = lazy(() => import('./pages/Projects'));
 
-
-
+const persistor = persistStore(store); 
 
 
 const App = () => {
 
   return (
     <>
-      <PersistGate persistor={persistStore(store)}>
-        <Provider store={store}>
+    <Analytics/>
+    <ReactLenis root options={{ lerp: 0.1, smoothWheel: true }}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}> 
           <ScrollToTop />
           <ParticlesComponent />
           <ThemeProvider>
             <Suspense fallback={<Spinner />}>
               <Routes>
-                <Route path='/' element={<Layout />} >
+                <Route path='/' element={<Layout />}>
                   <Route index element={<Home />} />
                   <Route path='/contact' element={<Contact />} />
                   <Route path='/projects' element={<Projects />} />
                   <Route path='/about' element={<About />} />
-             
                 </Route>
               </Routes>
             </Suspense>
           </ThemeProvider>
-        </Provider>
-        <Toaster />
-      </PersistGate>
-
-
-
-    </>
-  )
+          <Toaster />
+        </PersistGate>
+      </Provider>
+      </ReactLenis>
+      </>
+  );
 }
 
-export default App
+export default App;
